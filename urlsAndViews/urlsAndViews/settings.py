9b09341cbe423214dbebc7ead8329a2a0bf85d73
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2024-09-16 21:14:08 +0300

    Added more solutions

--- urlsAndViews/urlsAndViews/settings.py
+++ urlsAndViews/urlsAndViews/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse, reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z(=bk&ro@o0i19-i4+lkj#=gt()56vah#*hnrdbv95ru6kp_kq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -123,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = reverse_lazy('redirect-view', kwargs={"pk": 10})
